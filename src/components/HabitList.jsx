516e755ec4df84dff91bf7f41d0a3d1070c9e435
Shashi5657 <sangepushashidhar@gmail.com>
2024-10-16 13:49:59 +0530

    habit tracker

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,4 +1,4 @@
-const HabitList = ({ habits, toggleCompletion, deleteHabit }) => {
+const HabitList = ({ habits, toggleCompletion, deleteHabit, setReminder }) => {
   return (
     <div className="habit-list">
       {habits.map((habit) => (
@@ -9,6 +9,7 @@ const HabitList = ({ habits, toggleCompletion, deleteHabit }) => {
           <span onClick={() => toggleCompletion(habit.id)}>{habit.name}</span>
           <span className="streak">Streak: {habit.streak}</span>
           <button onClick={() => deleteHabit(habit.id)}>Delete</button>
+          <button onClick={() => setReminder(habit.id)}>Set Reminder</button>
         </div>
       ))}
     </div>
